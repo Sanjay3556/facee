134792fe1a687019fbd9201962313269ab33c019
Sanjay3556 <110145049+Sanjay3556@users.noreply.github.com>
2022-08-20 10:59:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -115,7 +115,7 @@ html_temp = """
    <div class="fixed-bottom ">
       <div class="dark bg-dark " style="min-height: 40px;">
          <marquee style="color:#fff; margin-top: 7px;">
-            <h9>Designed & Developed by Kishor Kumar, Student of Poornima Institute of Engineering and Technology</h9>
+            <h9>Designed & Developed by Sanjay Singh, Student of Poornima Colllege  of Engineering</h9>
          </marquee>
       </div>
    </div>
